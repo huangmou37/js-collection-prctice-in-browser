160126f297449e9f7ff76c88114de9cbd7803f3d
Cheng Huang <cheng.huang2@exxonmobil.com>
2020-07-08 00:27:06 +0800

    Implement collectSameElements in section-2/practice-3

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,41 @@
 'use strict';
 
 function countSameElements(collection) {
-  return 'Implement the practice require, and begin changing code in this row';
+  let countResultDict = {};
+
+  collection.forEach(e => {
+    let val = e.charAt(0);
+    let cnt = parseCnt(e);
+
+    if (val in countResultDict) {
+      countResultDict[val] += cnt;
+    } else {
+      countResultDict[val] = cnt;
+    }
+  });
+
+  let countResultArray = [];
+
+  for (let e in countResultDict) {
+    countResultArray.push({name: e, summary: countResultDict[e]});
+  }
+
+  return countResultArray;
+}
+
+function parseCnt(element) {
+  let cnt;
+
+  if (element.length === 1) {
+    cnt = 1;
+  } else {
+    let sep = element.charAt(1);
+    if ([':', '-'].includes(sep)) {
+      cnt = element.substring(2)
+    } else if (sep === '[') {
+      cnt = element.substring(2, element.length-1)
+    }
+  }
+
+  return parseInt(cnt);
 }
