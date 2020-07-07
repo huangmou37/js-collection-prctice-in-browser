0d732e2db94228ab7b9294a06037ed7e848e0022
Cheng Huang <cheng.huang2@exxonmobil.com>
2020-07-08 01:40:54 +0800

    Refactor collectSameElements in section-3/practice-4

--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -1,5 +1,40 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  let countResult = getCountResult(collectionA);
+
+  return Object.entries(countResult).map(entry => {
+    let newEntry = { key: entry[0], count: entry[1] };
+    if (objectB.value.includes(newEntry.key)) {
+      newEntry.count -= subOneEachThree(newEntry.count)
+    }
+    return newEntry;
+  });
 }
+
+function getCountResult(collectionA) {
+  return collectionA.reduce((accCountDict, e) => {
+    let key = e.charAt(0);
+    let cnt = parseCnt(e);
+
+    if (key in accCountDict) {
+      accCountDict[key] += cnt;
+    } else {
+      accCountDict[key] = cnt;
+    }
+    return accCountDict;
+  }, {});
+}
+
+function parseCnt(element) {
+  if (element.length === 1) {
+    return 1;
+  } else {
+    return parseInt(element.substring(2));
+  }
+}
+
+function subOneEachThree(cnt) {
+  return Math.floor(cnt / 3);
+}
+
